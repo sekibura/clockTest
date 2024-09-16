9f04efe0eefaad14add6a916aab0a23df750e764
Petrov Dmitry <petrovdmtry@gmail.com>
2024-09-16 19:26:50 +0300

    added analog watch

--- clockTest/Assets/Scripts/UI/ConcreteViews/MainClockView.cs
+++ clockTest/Assets/Scripts/UI/ConcreteViews/MainClockView.cs
@@ -13,9 +13,19 @@ namespace Sekibura.ClockInterview.UI
     public class MainClockView : View
     {
         [SerializeField]
-        private TMP_Text _digitalWatch;
-        [SerializeField]
         private Button _syncTimeBtn;
+
+
+        [Header("Digital")]
+        [SerializeField]
+        private TMP_Text _digitalWatch;
+        [Header("Analog")]
+        [SerializeField]
+        private GameObject _hourArrow;
+        [SerializeField]
+        private GameObject _minuteArrow;
+        [SerializeField]
+        private GameObject _secondsArrow;
 
         private TimeController _timeController;
         public override void Initialize()
@@ -34,6 +44,7 @@ namespace Sekibura.ClockInterview.UI
         private void UpdateWatches(DateTime dateTime)
         {
             UpdateDigitalWatch(dateTime);
+            UpdateAnalogWatches(dateTime);
         }
 
         private void UpdateDigitalWatch(DateTime dateTime)
@@ -44,5 +55,20 @@ namespace Sekibura.ClockInterview.UI
             });
         }
 
+        private void UpdateAnalogWatches(DateTime currentTime)
+        {
+            UnityMainThreadDispatcher.Instance().Enqueue(() =>
+            {
+                float hoursAngle = (currentTime.Hour % 12 + currentTime.Minute / 60f) * 30f - 90;  // 360 градусов / 12 часов = 30 градусов на час
+                _hourArrow.transform.localRotation = Quaternion.Euler(0, 0, -hoursAngle);
+
+                float minutesAngle = (currentTime.Minute + currentTime.Second / 60f) * 6f - 90;    // 360 градусов / 60 минут = 6 градусов на минуту
+                _minuteArrow.transform.localRotation = Quaternion.Euler(0, 0, -minutesAngle);
+
+                float secondsAngle = currentTime.Second * 6f - 90;                                 // 360 градусов / 60 секунд = 6 градусов на секунду
+                _secondsArrow.transform.localRotation = Quaternion.Euler(0, 0, -secondsAngle);
+
+            });
+        }
     }
 }
